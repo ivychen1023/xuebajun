da69ef592c5f6e268bb7c8d8504e645b9211cd8f
lishuting1019 <37200447+lishuting1019@users.noreply.github.com>
2018-05-16 15:25:12 +0800

    Update util.py

--- core/util.py
+++ core/util.py
@@ -6,5 +6,12 @@ Created on 2018-04-13
 """
 
 
-def sdk():
-    return  sdcdcd
+def get_denominator(expr,x = symbols('x')):
+    denominator = None
+    if expr.func.__name__ == 'Pow' and expr.args[1] == -1:
+            return expr.args[0]
+    if expr.func.__name__ == 'Mul':
+        for sub_expr in expr.args:
+            if sub_expr.func.__name__ == 'Pow' and sub_expr.args[1] == -1:
+                return sub_expr.args[0]
+    return denominator
